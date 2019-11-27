f8b595a30d65763555bff35aad8db0487764cf81
Gábor Csárdi <csardi.gabor@gmail.com>
2019-11-27 01:44:33 +0000

    Lazy Rmd building
    This is experimental. The goal is to (re)build the Rmds
    (and re-run the code) in exactly the environments we want to.
    
    In particular, for this package:
    - locally only if the Rmd file has changed
    - on CI only if recording is supported
    - on CRAN never.

--- R/onload.R
+++ R/onload.R
@@ -40,5 +40,13 @@ env_file <- NULL
 
   env_file <<- tempfile()
   saveRDS(env, file = env_file, version = 2, compress = FALSE)
+
+  lazyrmd$onload_hook(
+    .packageName,
+    local = "if-newer",
+    ci = function() is_recording_supported(),
+    cran = FALSE
+  )
+
   invisible()
 }
