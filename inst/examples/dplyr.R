a7f5cba63c38640c656524fcf2c3b2d790f07e7e
Gábor Csárdi <csardi.gabor@gmail.com>
2025-04-29 12:48:50 +0200

    Switch to the base pipe

--- inst/examples/dplyr.R
+++ inst/examples/dplyr.R
@@ -7,16 +7,16 @@
 
 library(dplyr)
 
-iris %>%
-  dplyr::group_by(Species) %>%
+iris |>
+  dplyr::group_by(Species) |>
   dplyr::summarise_all(mean)
 
 # Here is another command:
 
-starwars %>%
-  group_by(species) %>%
+starwars |>
+  group_by(species) |>
   summarise(
     n = n(),
     mass = mean(mass, na.rm = TRUE)
-  ) %>%
+  ) |>
   filter(n > 1, mass > 50)
